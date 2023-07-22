8a5801039d96e3f138c3d6175c9936b6d58f0ca3
Amjad Jibon <amjad.jibon@gmail.com>
2023-07-22 14:04:01 +0600

    Config object

--- src/conf/env.rs
+++ src/conf/env.rs
@@ -1,4 +1,37 @@
 use dotenv;
+use lazy_static::lazy_static;
+
+pub struct Config {
+    pub host: String,
+    pub port: String,
+    pub db_dsn: String,
+    pub jwt_secret: String,
+    pub jwt_access_exp: i64,
+    pub jwt_refresh_exp: i64,
+    pub jwt_aud: String,
+    pub jwt_iss: String,
+    pub jwt_sub: String,
+}
+
+lazy_static!(
+    pub static ref CONFIG: Config = Config::init();
+);
+
+impl Config {
+    pub fn init() -> Config {
+        Config{
+            host: get("HOST"),
+            port: get("PORT"),
+            db_dsn: get("DB_DSN"),
+            jwt_secret: get("JWT_SECRET"),
+            jwt_access_exp: get("JWT_ACCESS_EXP").parse::<i64>().unwrap(),
+            jwt_refresh_exp: get("JWT_REFRESH_EXP").parse::<i64>().unwrap(),
+            jwt_aud: get("JWT_AUD"),
+            jwt_iss: get("JWT_ISS"),
+            jwt_sub: get("JWT_SUB"),
+        }
+    }
+}
 
 pub fn init() {
     dotenv::dotenv().ok().expect("failed to load .env file");
